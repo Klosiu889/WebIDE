f768a42cc2a1b6aa98f26e22a64f0288ceb0ef87
Jakub Kłos <93655086+Klosiu889@users.noreply.github.com>
2023-03-20 18:46:06 +0100

    Little improvements

--- Scripts/Images.js
+++
@@ -1,7 +0,0 @@
-let fileImageURL = "Images/mac-file.png";
-let fileImgElements = document.querySelectorAll('.file');
-fileImgElements.forEach(imgElement => imgElement.src = fileImageURL);
-
-let folderImageURL = "Images/mac-folder.png";
-let folderImgElements = document.querySelectorAll('.folder');
-folderImgElements.forEach(imgElement => imgElement.src = folderImageURL);
