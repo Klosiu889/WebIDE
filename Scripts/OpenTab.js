d26f7ee64b4c22c56221296fd253f460ed6190a2
Jakub Kłos <93655086+Klosiu889@users.noreply.github.com>
2023-04-14 11:45:02 +0200

    Little improvements

--- Scripts/OpenTab.js
+++ Scripts/OpenTab.js
@@ -1,5 +1,30 @@
+const tabContent = document.getElementsByClassName("options");
+const processorOptions = document.getElementsByClassName("processor-radio");
+
+for (let i = 0; i < processorOptions.length; i++) {
+    processorOptions[i].addEventListener("click", (evt) => changeDependant(evt));
+}
+
+function changeDependant(evt) {
+    const processorOptionID = evt.target.id;
+    const dependentTab = document.getElementById("dependent-button");
+
+    switch (processorOptionID) {
+        case "mcs51":
+            dependentTab.onclick = function() {openTab(event, 'dependent-mcs51')};
+            break;
+        case "z80":
+            dependentTab.onclick = function() {openTab(event, 'dependent-z80')};
+            break;
+        case "stm8":
+            dependentTab.onclick = function() {openTab(event, 'dependent-stm8')};
+            break;
+        default:
+            dependentTab.onclick = function() {openTab(event, 'dependent')};
+    }
+}
+
 function hideTabs() {
-    const tabContent = document.getElementsByClassName("options");
     for (let i = 0; i < tabContent.length; i++) {
         tabContent[i].style.display = "none";
     }
@@ -7,17 +32,7 @@ function hideTabs() {
 
 function openTab(evt, tabName) {
     hideTabs();
-
-    const tabContent = document.getElementsByClassName("options");
-    const tablinks = document.getElementsByClassName("tablinks");
-    for (let i = 0; i < tabContent.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
     document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-
-    console.log("Tab opened");
 }
 
 openTab(event, 'standard');
